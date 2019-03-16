be459fcdb8598b25bb503d4e00978f46617f510d
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-03-16 10:40:37 -0400

    Pull game directory from op2ext
    This allows attaching a debugger and still finding the correct location of the .ini file. Otherwise if the debugger is attached, the default protocol index of 4 is returned (because .ini file is not found) which makes it appear as if the module is not loading properly.
    
    I wanted to find a solution that would allow Outpost 2 to be buried in a directory deeper than MAX_PATH, but didn't come up with anything useful.

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,4 @@
+#include "op2ext.h"
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 #include <windows.h>
 
@@ -8,6 +9,8 @@ using namespace OP2Internal;
 #include "OPUNetGameProtocol.h"
 
 #include <fstream>
+#include <cstddef>
+#include <string>
 extern std::ofstream logFile;
 
 
@@ -36,6 +39,8 @@ BOOL APIENTRY DllMain(HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserv
     return TRUE;
 }
 
+std::string GetOutpost2Directory();
+
 extern "C" __declspec(dllexport) void InitMod(char* iniSectionName)
 {
 	// Sanity check the DLL load address
@@ -57,10 +62,24 @@ extern "C" __declspec(dllexport) void InitMod(char* iniSectionName)
 	// Store the .ini section name
 	strncpy(sectionName, iniSectionName, sizeof(sectionName));
 
-	int protocolIndex;
-	// Get button index to replace functionality of
-	protocolIndex = GetPrivateProfileInt(sectionName, "ProtocolIndex", DefaultProtocolIndex, ".\\Outpost2.ini");
+
+	// Get multiplayer button index that NetFix will replace
+	const std::string iniPath = GetOutpost2Directory() + "Outpost2.ini"; 
+
+	int protocolIndex = GetPrivateProfileInt(sectionName, "ProtocolIndex", DefaultProtocolIndex, iniPath.c_str());
 	logFile << "[" << sectionName << "]" << " ProtocolIndex = " << protocolIndex << std::endl;
 	// Set a new multiplayer protocol type
 	protocolList[protocolIndex].netGameProtocol = &opuNetGameProtocol;
+}
+
+std::string GetOutpost2Directory()
+{
+	char buffer[MAX_PATH];
+	std::size_t bufferSize = GetGameDir_s(buffer, MAX_PATH);
+
+	if (bufferSize != 0) {
+		throw std::runtime_error("Absolute directory to Outpost 2 must be less than " + MAX_PATH);
+	}
+
+	return std::string(buffer);
 }
