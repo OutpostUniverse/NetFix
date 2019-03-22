41f7b62ec1752cbcf0d0c949f3ca0e163d742cdd
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-03-22 19:17:49 +0700

    Add public prototypes for all Format methods

--- Log.h
+++ Log.h
@@ -14,8 +14,12 @@ namespace OP2Internal {
 extern std::ofstream logFile;
 
 
+std::string FormatAddress(const sockaddr_in& address);
 std::string FormatIP4Address(unsigned long ip);
+std::string FormatPlayerList(const PeerInfo* peerInfo);
 std::string FormatPlayerNetID(int playerNetID);
+std::string FormatGuid(const GUID& guid);
+std::string FormatPacket(const OP2Internal::Packet& packet);
 
 
 void Log(const char* string);
