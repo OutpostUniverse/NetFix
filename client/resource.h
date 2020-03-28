435e5befca8a007600dcd35cf5b675001c07eca3
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-03-28 19:51:06 -0400

    Add a tool tip to represent server address
    Currently, tool tip shows when the server address label is under the cursor. It doesn't seem to work when applied over the server address combo box

--- client/resource.h
+++ client/resource.h
@@ -15,6 +15,7 @@
 #define IDC_ServerAddress               1014
 #define IDC_StatusBar                   1018
 #define IDC_NetInfo                     1020
+#define IDC_ServerAddressLabel          1021
 
 // Next default values for new objects
 // 
@@ -22,7 +23,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_CONTROL_VALUE         1022
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
